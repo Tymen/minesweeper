28c9de09bba8a610858ce9c1755ae35004ffd2b5
Tymen <Tymenvis@outlook.com>
2021-03-09 19:33:07 +0100

    Almost fully implemented the core system

--- src/views/Minesweeper.vue
+++ src/views/Minesweeper.vue
@@ -1,42 +1,16 @@
 <template>
   <div class="home">
     <h1>Minesweeper</h1>
+    <label for="bombs">Bombs</label>
+    <input name="bombs" id="bombs" :value="bombCount"><br>
+    <label for="rows">Rows</label>
+    <input name="rows" id="rows" :value="rows"><br>
+    <label for="collumns">collumns</label>
+    <input name="collumns" id="collumns" :value="collumns"><br>
     <button @click="generateField">Generate</button>
     <div @contextmenu="disableRClick($event)" class="container">
-        <div id="y-0" class="row">
-            <div id="0,0" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="0,1" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="0,2" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="0,3" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="0,4" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-        </div>
-        <div id="y-1" class="row">
-            <div id="1,0" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="1,1" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="1,2" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="1,3" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="1,4" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-        </div>
-        <div id="y-2" class="row">
-            <div id="2,0" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="2,1" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="2,2" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="2,3" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="2,4" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-        </div>
-        <div id="y-3" class="row">
-            <div id="3,0" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="3,1" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="3,2" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="3,3" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="3,4" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-        </div>
-        <div id="y-4" class="row">
-            <div id="4,0" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="4,1" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="4,2" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="4,3" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
-            <div id="4,4" @contextmenu="markBomb($event)" v-on:click="check($event)" class="hidden"></div>
+        <div v-for="row in rows" :key="row" :id="`y-${row - 1}`" class="row">
+            <div v-for="col in collumns" :key="col" :id="`${row - 1},${col -1}`" @contextmenu="markBomb($event)" v-on:click="clickTile($event)" class="hidden"></div>
         </div>
     </div>
   </div>
@@ -48,8 +22,11 @@ export default {
     name: 'Minesweeper',
     data () {
       return {
-          bombCount: 0,
+          bombCount: 10,
           bombLocations: [],
+          rows: 6,
+          collumns: 6,
+          mapObject: {},
       }
     },
     components: {
@@ -61,76 +38,118 @@ export default {
             let element = document.getElementById(e.srcElement.id);
             element.classList.contains("flag") ? element.classList.remove("flag") : element.classList.add("flag");
         },
-        check(e) {
-            let bombs = 0;
-            let tile = JSON.parse("[" + e.srcElement.id + "]");
-            let element = document.getElementById(e.srcElement.id);
-            if (this.bombLocations.includes(`${tile[0]},${tile[1]}`)){
-                element.classList.add("bomb");
-                element.classList.remove("flag");
+        clickTile(e){
+            let el = document.getElementById(e.srcElement.id);
+            if(this.bombLocations.includes(e.srcElement.id)){
+                this.revealBombs()
+                el.classList.remove("flag");
+            }else {
+                el.innerText = this.mapObject[e.srcElement.id]
+                this.checkForZero(e.srcElement.id)
             }
+        },
+        checkForZero(cords) {
+            let tile = JSON.parse("[" + cords + "]");
             for(let i = 0; i < 8; i++){
-                switch(i){
-                    case 0:
-                        tile[0] = tile[0] - 1;
-                        break;
-                    case 1:
-                        tile[1] = tile[1] + 1;
-                        break;
-                    case 2:
-                        tile[0] = tile[0] + 1;
-                        break;
-                    case 3:
-                        tile[0] = tile[0] + 1;
-                        break;
-                    case 4:
-                        tile[1] = tile[1] - 1;
-                        break;
-                    case 5:
-                        tile[1] = tile[1] - 1;
-                        break;
-                    case 6: 
-                        tile[0] = tile[0] - 1;
-                        break;
-                    case 7:
-                        tile[0] = tile[0] - 1;
-                        break;
+                console.log(`${tile[0]},${tile[1]}`);
+                tile = this.getCordsAroundTile(i, tile)
+                if(this.mapObject[`${tile[0]},${tile[1]}`] == 0 && !this.bombLocations.includes(`${tile[0]},${tile[1]}`)){
+                    document.getElementById(`${tile[0]},${tile[1]}`).innerText = this.mapObject[`${tile[0]},${tile[1]}`];
                 }
+            }
+        },
+        revealBombs() {
+            for(let i = 0; i < this.bombLocations.length; i++){
+                document.getElementById(this.bombLocations[i]).classList.add("bomb")
+            }
+        },
+        getCordsAroundTile(i, tile) {
+            switch(i){
+                case 0:
+                    tile[0] = tile[0] - 1;
+                    break;
+                case 1:
+                    tile[1] = tile[1] + 1;
+                    break;
+                case 2:
+                    tile[0] = tile[0] + 1;
+                    break;
+                case 3:
+                    tile[0] = tile[0] + 1;
+                    break;
+                case 4:
+                    tile[1] = tile[1] - 1;
+                    break;
+                case 5:
+                    tile[1] = tile[1] - 1;
+                    break;
+                case 6: 
+                    tile[0] = tile[0] - 1;
+                    break;
+                case 7:
+                    tile[0] = tile[0] - 1;
+                    break;
+            }
+            return tile;
+        },
+        checkBombsAround(cords) {
+            let bombs = 0;
+            let tile = JSON.parse("[" + cords + "]");
+            for(let i = 0; i < 8; i++){
+                tile = this.getCordsAroundTile(i, tile)
                 console.log(`${tile[0]},${tile[1]}`);
                 if (this.bombLocations.includes(`${tile[0]},${tile[1]}`)){
                     bombs = bombs + 1;
                     console.log(`Bomblocation: ${tile[0]},${tile[1]}`);
                 }
             }
-            console.log("there are" + bombs + "around")
-            element.innerText = bombs;
-            console.log("you clicked " + e.srcElement.id)
+            return bombs;
         },
         disableRClick(e) {
             e.preventDefault();
         },
         generateField() {
-            console.log(this.bombCount);
+            if (document.getElementById("bombs").value && document.getElementById("rows").value && document.getElementById("collumns").value) {
+                this.bombCount = parseInt(document.getElementById("bombs").value)
+                this.rows = parseInt(document.getElementById("rows").value);
+                this.collumns = parseInt(document.getElementById("collumns").value);
+            }
+            this.bombLocations = [];
+            this.mapObject = {};
+            this.setBombLocations();
+            this.checkTiles();
         },
-        generateCords() {
-            return `${Math.floor(Math.random() * 5)},${Math.floor(Math.random() * 5)}`;
-        }
-    },
-    created() {
-        this.bombCount = 4;
-        for(let i = 0; i < this.bombCount; i++){
-            let cords = this.generateCords()
+        checkTiles() {
+            for(let y = 0; y < this.rows; y++){
+                for(let x = 0; x < this.collumns; x++){
+                    var el = document.getElementById(`${y},${x}`);
+                    el.classList.remove("bomb","flag")
+                    el.innerText = "";
+                    this.mapObject[`${y},${x}`] = this.checkBombsAround(`${y},${x}`)
+                }
+            }
+        },
+        setBombLocations(){
+            for(let i = 0; i < this.bombCount; i++){
 
+            let cords = this.generateCords()
             let loop = false;
             this.bombLocations.includes(cords) ? loop = true : loop = false;
+
             while(loop) {
                 cords = this.generateCords()
                 this.bombLocations.includes(cords) ? loop = true : loop = false;
             }
 
             this.bombLocations.push(cords); 
-            console.log(cords)
         }
+        },
+        generateCords() {
+            return `${Math.floor(Math.random() * this.rows)},${Math.floor(Math.random() * this.collumns)}`;
+        }
+    },
+    mounted() {
+        this.generateField();
     }
 }
 </script>
@@ -151,10 +170,18 @@ export default {
     padding: 10px;
     width: 40px;
     height: 40px;
-    margin: 5px;
+    margin: 3px;
+    cursor: pointer;
+    transition: .2s;
+}
+.hidden:hover {
+    background-color: rgb(94, 94, 94);
 }
 .bomb {
     background-color: red;
+}
+.bomb:hover {
+    background-color: red!important;
 }
 .flag {
     background-color: rgba(253, 0, 219, 0.596)!important;
